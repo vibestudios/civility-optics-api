ce1076612313236103b583c877b52388bd496442
Nabil Patel <npatel412@gatech.edu>
2021-10-03 16:36:55 -0400

    The POST request returns the average of the ratings for the business ID

--- backend/controllers/ratings_controller.js
+++ backend/controllers/ratings_controller.js
@@ -6,6 +6,27 @@ export const postRating = asyncHandler(async(req, res) => {
         console.log(req.body)
         const rating = new Ratings(req.body)
         await rating.save()
+
+
+        const pipeline = [
+            {
+              '$match': {
+                'place_id': req.body.place_id
+              }
+            }, {
+              '$group': {
+                '_id': '$place_id', 
+                'avg_rating': {
+                  '$avg': '$value'
+                }
+              }
+            }
+        ]
+        const ratings = await Ratings.aggregate(pipeline)
+        res.json(ratings)
+
+
+        
         res.status(200).send(rating)
     } catch (error) {
         res.status(400).send(error)
@@ -14,7 +35,6 @@ export const postRating = asyncHandler(async(req, res) => {
 
 export const getRating = asyncHandler(async(req, res) => {
     try {
-        console.log(req.body.place_id)
         const pipeline = [
             {
               '$match': {
